dd6887774b6807f51a67c505fbf4d57ece66a645
Andrea de Anda <adaku.andy@gmail.com>
2020-11-08 23:09:25 -0600

    Tarea BG

---
+++ TareaBG/tareaBG1.asm
@@ -0,0 +1,66 @@
+INCLUDE \masm32\Irvine\Irvine32.inc
+INCLUDELIB \masm32\Irvine\Irvine32.lib
+INCLUDELIB \masm32\Irvine\User32.lib
+INCLUDELIB \masm32\Irvine\Kernel32.lib
+
+.DATA
+A SDWORD 7
+B SDWORD ?
+D SDWORD -15
+R SDWORD ?
+texto BYTE "DATO:", 0
+textor BYTE "El resultado R=", 0
+textorh BYTE "El resultado Rh=", 0
+textofin BYTE "HASTA LA VISTA",0
+
+.CODE 
+main PROC
+    mov EDX, OFFSET texto
+    call WriteString
+    call CrLf
+    call ReadInt
+    mov B, EAX
+
+    ; -A*9
+    mov EAX, A
+    neg EAX
+    mov EBX, 0
+    mov BX,9
+    imul EBX
+    mov ECX, EAX
+
+    ;(B/D)+1
+    mov EAX, B
+    mov EBX, D
+    mov EDX, 0; por que idiv deja el residuo en EDX
+    idiv EBX
+    inc EAX
+
+    ;-A*9-(B/D+1)
+    sub ECX, EAX
+    mov EAX, ECX
+
+    ;-A*9-(B/D+1)+100
+    add EAX,100
+    
+ 
+    mov EDX, OFFSET textor
+    call CrLf
+    call WriteString
+    call WriteInt
+    call CrLf
+    mov EDX, OFFSET textorh
+    call CrLf
+    call WriteString
+    call WriteHex
+    call CrLf
+    call DumpRegs
+    mov EDX, OFFSET textofin
+    call CrLf
+    call WriteString
+    exit
+main ENDP
+
+END main
+
+END main
---
+++ TareaBG/tareaBG2.asm
@@ -0,0 +1,43 @@
+INCLUDE \masm32\Irvine\Irvine32.inc
+INCLUDELIB \masm32\Irvine\Irvine32.lib
+INCLUDELIB \masm32\Irvine\User32.lib
+INCLUDELIB \masm32\Irvine\Kernel32.lib
+
+.DATA
+Svector WORD 2002h, 4004h, 6006h, 8008h
+Cvector SWORD -2, -4, -6, -8
+textoS BYTE "Suma:", 0
+textoP BYTE "Producto:", 0
+textofin BYTE "ADIOS", 0 
+
+.CODE
+main PROC
+
+    movzx EAX, Svector+6
+    movzx EBX, Cvector+6
+    ADD EAX,EBX
+    mov EDX, OFFSET textoS
+    call WriteString
+    call WriteInt
+    call CrLf
+
+    movzx EAX, Svector+4
+    movzx EBX, Cvector+4
+    imul EBX
+    call CrLf
+    mov EDX, OFFSET textoP
+    call WriteString
+    call WriteInt
+    call CrLf
+
+    call CrLf
+    mov EDX, OFFSET textofin
+    call WriteString
+    call CrLf
+
+
+    exit
+
+main ENDP
+
+END main
